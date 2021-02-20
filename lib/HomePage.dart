02858e820364ab899723b4a1f8ebb82993f8fe2e
prathamesh22davande <prathamesh22davande@gmail.com>
2021-02-20 22:55:23 +0530

    Completed UI part

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'Employee.dart';
+import 'EmployeeBloc.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +8,79 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final EmployeeBloc _employeeBloc = EmployeeBloc();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _employeeBloc.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Employee App"),
+      ),
+      body: Container(
+        child: StreamBuilder<List<Employee>>(
+          stream: _employeeBloc.employeeListStream,
+          builder:
+              (BuildContext context, AsyncSnapshot<List<Employee>> snapshot) {
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                      elevation: 5.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(20),
+                            child: Text(
+                              "${snapshot.data[index].id}",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(20),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "${snapshot.data[index].name}",
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  Text(
+                                    "₹ ${snapshot.data[index].salary}",
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ]),
+                          ),
+                          Container(
+                            child: IconButton(
+                              icon: Icon(Icons.thumb_up,),
+                              color: Colors.green,
+                              onPressed: (){
+                                _employeeBloc.employeeSalaryIncrement.add(snapshot.data[index]);
+                              },
+                            ),
+                          ),
+                          Container(
+                            child: IconButton(
+                              icon: Icon(Icons.thumb_down,),
+                              color: Colors.red,
+                              onPressed: (){
+                                _employeeBloc.employeeSalaryDecrement.add(snapshot.data[index]);
+                              },
+                            ),
+                          ),
+                        ],
+                      ));
+                });
+          },
+        ),
+      ),
+    );
   }
 }
