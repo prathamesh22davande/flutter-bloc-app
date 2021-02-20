c8067a7f9d2e3424eddadd68d14d3618a259764f
prathamesh22davande <prathamesh22davande@gmail.com>
2021-02-20 22:34:08 +0530

    7 steps for bloc

---
+++ lib/EmployeeBloc.dart
@@ -0,0 +1,68 @@
+//TODO: imports
+import 'dart:async';
+import 'Employee.dart';
+
+//TODO: List of employees
+class EmployeeBloc{
+
+  List<Employee> _employeeList = [
+    Employee(1,"Employee One",10000.0),
+    Employee(2,"Employee two",20000.0),
+    Employee(3,"Employee three",50000.0),
+    Employee(4,"Employee four",30000.0),
+    Employee(5,"Employee five",40000.0),
+  ];
+
+  //TODO: Stream controllers
+  final _employeeListStreamController = StreamController<List<Employee>>();
+
+  //for inc and dec
+  final _employeeSalaryIncrementStreamController = StreamController<Employee>();
+  final _employeeSalaryDecrementStreamController = StreamController<Employee>();
+
+  //TODO: Stream Sink getter
+  Stream<List<Employee>> get employeeListStream => _employeeListStreamController.stream;
+  StreamSink<List<Employee>> get employeeListSink => _employeeListStreamController.sink;
+
+  StreamSink<Employee> get employeeSalaryIncrement => _employeeSalaryIncrementStreamController.sink;
+  StreamSink<Employee> get employeeSalaryDecrement => _employeeSalaryDecrementStreamController.sink;
+
+
+  //TODO: Constructor - add data; listen to changes
+  EmployeeBloc(){
+    _employeeListStreamController.add(_employeeList);
+
+    _employeeSalaryIncrementStreamController.stream.listen(_incrementSalary);
+    _employeeSalaryDecrementStreamController.stream.listen(_decrementSalary);
+  }
+
+  //TODO: Core functions
+  _incrementSalary(Employee employee){
+    double salary = employee.salary;
+    double incrementedSalary = salary * 20/100;
+    _employeeList[employee.id - 1].salary = salary+incrementedSalary;
+    employeeListSink.add(_employeeList);
+  }
+
+  _decrementSalary(Employee employee){
+    double salary = employee.salary;
+    double decrementedSalary = salary * 20/100;
+    _employeeList[employee.id - 1].salary = salary-decrementedSalary;
+    employeeListSink.add(_employeeList);
+  }
+
+
+  //TODO: dispose
+  void dispose(){
+    _employeeSalaryIncrementStreamController.close();
+    _employeeSalaryDecrementStreamController.close();
+    _employeeListStreamController.close();
+  }
+}
+
+
+
+
+
+
+
